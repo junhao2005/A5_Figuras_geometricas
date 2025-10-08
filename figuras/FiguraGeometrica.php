d755ec2e14148a4f657f987f6c0b1f270fee2ace
junhao2005 <145134984+junhao2005@users.noreply.github.com>
2025-10-08 23:58:20 +0200

    carpea figuras + ficheros de figuras

---
+++ figuras/FiguraGeometrica.php
@@ -0,0 +1,29 @@
+<?php
+
+abstract class FiguraGeometrica {
+    protected $tipoFigura;
+    protected $lado1;
+
+    public function __construct($tipoFigura, $lado1) {
+        $this->tipoFigura = $tipoFigura;
+        $this->lado1 = $lado1;
+    }
+
+    // Genera el constructor con todos sus atributos y el destructor
+    public function __destruct() {}
+
+
+    // ge
+    
+}
+    
+   
+
+
+
+
+
+
+
+
+?>
